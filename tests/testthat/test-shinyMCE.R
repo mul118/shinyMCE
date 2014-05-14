d216da4f51203004671ae7b34a600383d1c216a1
mul118 <MikhailLisovich@clearchannel.com>
2014-05-14 16:18:38 -0400

    Remove failing test

--- tests/testthat/test-shinyMCE.R
+++ tests/testthat/test-shinyMCE.R
@@ -3,5 +3,4 @@ context("shinyMCE")
 test_that('all package functions are present',{
   expect_true(is.function(tinyMCE))
   expect_true(is.function(updateTinyMCE))
-  expect_that(sin(pi / 3), equals(1))
 })
