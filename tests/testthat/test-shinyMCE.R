2bc8bf3bca149959ac75c0d4b22817c8153f42c2
mul118 <MikhailLisovich@clearchannel.com>
2014-05-14 16:01:49 -0400

    Refresh failing test

--- tests/testthat/test-shinyMCE.R
+++ tests/testthat/test-shinyMCE.R
@@ -3,5 +3,5 @@ context("shinyMCE")
 test_that('all package functions are present',{
   expect_true(is.function(tinyMCE))
   expect_true(is.function(updateTinyMCE))
-  expect_that(sin(pi / 4), equals(1))
+  expect_that(sin(pi / 3), equals(1))
 })
